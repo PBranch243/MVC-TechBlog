31422e20a5c04fff7505d1cc1b1dc84726103f4d
unknown <philbranch243@gmail.com>
2022-09-18 16:05:37 -0500

    figured how to get pages displaying, had routes in wrong files, see comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
@@ -29,7 +30,7 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-
+      console.log(posts);
       res.render('homepage', {
         posts,
         loggedIn: req.session.loggedIn
