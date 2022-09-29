d29dacaf92187338daea5928d73bec75fc8fabeb
unknown <philbranch243@gmail.com>
2022-09-29 06:18:29 -0500

    edit post page working.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -98,10 +98,7 @@ router.get('/login', (req, res) => {
 });
 
 //route to render the dashboard
-//Add route to return all of a users posts and pass it to the render on line 104
-
-
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', withAuth, (req, res) => {
   Post.findAll({
     where: {
       // use the ID from the session
@@ -132,6 +129,52 @@ router.get('/dashboard', (req, res) => {
       // serialize data before passing to template
       const posts = dbPostData.map(post => post.get({ plain: true }));
       res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//route for edit page
+router.get('/dashboard/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_text',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const posts = dbPostData.get({ plain: true });
+
+      res.render('edit-post', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
